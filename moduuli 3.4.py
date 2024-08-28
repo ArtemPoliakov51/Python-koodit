737d72b4520f8ee66c0cbca58fcd2a7accb334f8
artem <ArtemPol@metropolia.fo>
2024-08-28 22:57:34 +0300

    moduuli 3

--- moduuli 3.4.py
+++ moduuli 3.4.py
@@ -1,5 +1,5 @@
 command = int(input("syötä vuosi: "))
-if (command % 4 == 0) and not command == 0:
+if (command % 4 == 0 and command % 100 != 0) or (command % 400 == 0) :
     print("Karkausvuosi")
 else:
     print("Ei karkausvuosi")
