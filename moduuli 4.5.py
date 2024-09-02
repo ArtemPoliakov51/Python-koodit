7707655a6af7d078f2863e7c8c76d3afc18d47ff
artem <ArtemPol@metropolia.fo>
2024-09-02 21:48:50 +0300

    moduuli 4

--- moduuli 4.5.py
+++ moduuli 4.5.py
@@ -1,8 +1,6 @@
 eka = input("syötä käyttäjätunnus: ")
 toka = input("syötä salasana: ")
 kolmas = eka != "python" and toka != "rules"
-if kolmas:
-    print("väärin käyttäjätunnus tai salasana\nyritä uudelleen!")
 while kolmas<5:
     if eka == "python" and toka == "rules":
         print("tervetuloa")
@@ -10,8 +8,6 @@ while kolmas<5:
     elif eka != "python" or toka != "rules":
         eka = input("syötä käyttäjätunnus: ")
         toka = input("syötä salasana: ")
-        print("väärin käyttäjätunnus tai salasana\nyritä uudelleen!")
     kolmas += 1
     if kolmas == 5:
         print("pääsy estetty")
-
