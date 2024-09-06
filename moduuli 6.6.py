d09003f6deabfc02bf06c83bac2112ee5d389ad1
artem <ArtemPol@metropolia.fo>
2024-09-07 02:08:22 +0300

    moduuli 6

--- moduuli 6.6.py
+++ moduuli 6.6.py
@@ -1,17 +1,17 @@
 def funktio(eka):
     toka = 3.14 * (eka/2) ** 2
     kolmas = toka/10000
-    neljäs = float(input("syötä hinta: "))
+    neljäs = float(input("Nyt syötä hinta: "))
     viides = neljäs/kolmas
     return viides
 
-ekas = float(input("syötä halkaisija"))
+ekas = float(input("Syötä ensimmäisen pizzan halkaisija: "))
 tokas = funktio(ekas)
-ekas2 = float(input("syötä toka halkaisija"))
+ekas2 = float(input("Syötä toisen halkaisija: "))
 tokas2 = funktio(ekas2)
 
 if tokas < tokas2:
-    print("eka edullisempi")
+    print("Ensimmäinen pizza on edullisempi")
 elif tokas2 < tokas:
-    print("toka edullisempi")
+    print("Toinen pizza on edullisempi")
 
