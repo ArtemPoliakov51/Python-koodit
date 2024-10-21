166b882814a2428c1c54e4befae3779bf3502b44
artem <ArtemPol@metropolia.fo>
2024-10-21 14:40:26 +0300

    moduuli 8

---
+++ asteista radiaaneihin.py
@@ -0,0 +1,7 @@
+import math
+
+pi = math.pi
+
+degrees = float(input("syötä asteina"))
+rad = degrees*pi/180
+print(rad)
---
+++ radiaaneista asteisiin.py
@@ -0,0 +1,10 @@
+import math
+
+pi = math.pi
+
+rad = float(input("syötä radiaaneina"))
+degrees = rad*180/pi
+print(degrees)
+
+
+
