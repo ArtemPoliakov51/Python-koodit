d25ea74751df486a22025e33b4d16fe60e1286a2
artem <ArtemPol@metropolia.fo>
2024-10-23 10:26:40 +0300

    moduuli 9

--- moduuli 9.1.py
+++ moduuli 9.1.py
@@ -1,11 +1,14 @@
 class Auto:
-    def __init__(self,tunnus,huippunopeus):
+    def __init__(self,tunnus,huippunopeus,hetkellinennopeus,matka):
         self.tunnus=tunnus
         self.huippunopeus=huippunopeus
+        self.hetkellinennopeus=hetkellinennopeus
+        self.matka=matka
 
-auto = Auto("ABC-123", 147)
+auto = Auto("ABC-123", 147,0,0)
 
-print(f"rekisteritunnus {auto.tunnus} ja huippunopeus {auto.huippunopeus}")
+print(f"rekisteritunnus {auto.tunnus}\nhuippunopeus {auto.huippunopeus}\nhetkellinen nopeus {auto.hetkellinennopeus}\nmatka {auto.matka}")
+
 
 
 
