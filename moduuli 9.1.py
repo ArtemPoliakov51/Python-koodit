e65e656aa1a6bf39a5d6d55d87568a4033f69065
artem <ArtemPol@metropolia.fo>
2024-10-23 10:22:03 +0300

    moduuli 9

---
+++ moduuli 9.1.py
@@ -0,0 +1,11 @@
+class Auto:
+    def __init__(self,tunnus,huippunopeus):
+        self.tunnus=tunnus
+        self.huippunopeus=huippunopeus
+
+auto = Auto("ABC-123", 147)
+
+print(f"rekisteritunnus {auto.tunnus} ja huippunopeus {auto.huippunopeus}")
+
+
+
